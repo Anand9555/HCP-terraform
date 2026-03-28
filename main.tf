ceeeb58e4076534c5811ba0d36b4a19849ced9bd
Anand <anandpathakoti18@gmail.com>
2026-03-28 11:36:58 +0530

    Rename AWS instance resource and update tags

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "one" {
-  count         = 1
+resource "aws_instance" "example" {
   ami           = "ami-077c6fac5ef663f46"
   instance_type = "t3.micro"
+
   tags = {
-    Name = "dev-server-${count.index+1}"
+    Name = "ANAND-instance"
   }
 }
